0381f46a31534b8865747f1a331beea81a92cb4e
denisjovic <denisjovic@pm.me>
2021-11-10 20:46:23 +0100

    test

--- test.php
+++ test.php
@@ -1,13 +1,13 @@
 <?php
 
 
-$conn = mysql_connect('localhost', 'root', 'password', 'testdb')
+$conn = mysql_connect('localhost', 'root', 'password', 'testdb');
 
 $getUser = $_REQUEST["username"];
 
 $query = "SELECT * FROM users WHERE username = '" .$getUser . "'";
 
-$result = mysql_query($conn, $query)
+$result = mysql_query($conn, $query);
 
 
 ?>
--- test2.php
+++ test2.php
@@ -1,6 +1,17 @@
 <?php
 
-$articleid = $_GET[‘article’];
-$query = "SELECT * FROM articles WHERE articleid = $articleid";
+function testSQLi() {
+	$conn = mysql_connect('localhost', 'root', 'password', 'testdb');
+
+	$articleid = $_GET[‘article’];
+	$query = "SELECT * FROM articles WHERE articleid = $articleid";
+
+	$result = mysql_query($conn, $query);
+};
+
+testSQLi();
+
+
+
 
 ?>
