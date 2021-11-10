6f3ddda0313c99e1aeb52ad355b9ee9f47edfb02
denisjovic <denisjovic@pm.me>
2021-11-10 20:19:30 +0100

    try pgquery

--- test.php
+++ test.php
@@ -1,11 +1,9 @@
 <?php
 
 
-$conn = mysqli_connect('localhost', 'root', 'password', 'users')
-
 $offset = $argv[0]; // beware, no input validation!
 $query  = "SELECT id, name FROM products ORDER BY name LIMIT 20 OFFSET $offset;";
-$result = mysqli_query($conn, $query))
+$result = pg_query($conn, $query);
 
 
 ?>
